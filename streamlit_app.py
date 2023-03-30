ef9473c0627f116a6c9528d9aea423df7236b1f4
Brent Schriver <90301809+brentschriver@users.noreply.github.com>
2023-03-29 21:24:31 -0500

    Update streamlit_app.py
    choose from 'Fruits' column instead of numbered index

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,4 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 streamlit.dataframe(my_fruit_list)
 
 # Create an interactive list for users to add fruits
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit))
