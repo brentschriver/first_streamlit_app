5c05d977a6678c559ddc561f4279ec2dcac38af8
Brent Schriver <90301809+brentschriver@users.noreply.github.com>
2023-03-29 21:29:29 -0500

    Update streamlit_app.py
    create dataframe of selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,7 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 streamlit.dataframe(my_fruit_list)
 
 # Create an interactive list for users to add fruits
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit))
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit))
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(fruits_to_show)
