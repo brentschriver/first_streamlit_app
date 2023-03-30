58a6bd9344e46aa19c66c855d054a402c8163704
Brent Schriver <90301809+brentschriver@users.noreply.github.com>
2023-03-29 21:18:48 -0500

    Update streamlit_app.py
    added list of fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("Our New Boutique")
 streamlit.header("Spring 2023")
 streamlit.text("Unisex Top")
 streamlit.text("Unisex Bottom")
 streamlit.text("Women Top")
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
